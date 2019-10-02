4220038dec57acbc6abe71ef2b021bf94a20e85f
GalenHu <1858105@johnabbottcollege.net>
2019-10-02 14:19:00 -0400

    Java Exercise

---
+++ 5.1-Beginner-JavaScriptProblems/main.js
@@ -0,0 +1,29 @@
+//1.
+/*
+function multiplyTwoNumbers( numA,  numB)
+{
+    return numA*numB;
+}
+
+let numA = prompt('What is the first multiplicator');
+let numB = prompt('What is the second multiplicator');
+
+let result = multiplyTwoNumbers(numA,numB);
+
+alert(`The answer is ${result}`);
+*/
+//End of 1.
+
+//2.
+/*
+let name=prompt(`What is your name`)
+let age=prompt(`How old are you`)
+sayHi()
+function sayHi()
+{
+    alert(`Hi. My name is ${name} and I'm ${age} years old`)
+}
+*/
+//End of 2.
+
+//3
