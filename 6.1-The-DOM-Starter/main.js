507b0cd025086816dcd822df82f3f9274d67893a
GalenHu <1858105@johnabbottcollege.net>
2019-10-15 13:53:56 -0400

    6.1 dom intro

---
+++ 6.1-The-DOM-Starter/main.js
@@ -0,0 +1,20 @@
+const mountains = [
+    {'name': 'Mount Everest', 'elevation': 8848, 'range': 'Himalayas', 'location': 'Nepal/China'},
+    {'name': 'K2', 'elevation': 8611, 'range': 'Karakoram', 'location': 'India - Highest in India'},
+    {'name': 'Kangchenjunga', 'elevation': 8586, 'range': 'Himalayas', 'location': 'Nepal/India'},
+    {'name': 'Lhotse', 'elevation': 8516, 'range': 'Himalayas', 'location': 'Nepal/China - Climbers ascend the face of Lhotse when climbing Everest'},
+    {'name': 'Makalu', 'elevation': 8485, 'range': 'Himalayas', 'location': 'Nepal/China'},
+    {'name': 'Cho Oyu', 'elevation': 8201, 'range': 'Himalayas', 'location': 'Nepal/China - Considered easiest eight-thousander'},
+    {'name': 'Dhaulagiri', 'elevation': 8167, 'range': 'Himalayas', 'location': 'Nepal - Presumed world\'s highest from 1808-1838'},
+    {'name': 'Manaslu', 'elevation': 8163, 'range': 'Himalayas', 'location': 'Nepal'},
+    {'name': 'Nanga Parbat', 'elevation': 8126, 'range': 'Himalayas', 'location': 'Pakistan'},
+    {'name': 'Annapurna', 'elevation': 8091, 'range': 'Himalayas', 'location': 'Nepal - First eight-thousander to be climbed (1950)'},
+    {'name': 'Gasherbrum I', 'elevation': 8080, 'range': 'Karakoram', 'location': 'Pakistan/China - Originally named K5'},
+    {'name': 'Broad Peak', 'elevation': 8051, 'range': 'Karakoram', 'location': 'Pakistan/China - Originally named K3'},
+    {'name': 'Gasherbrum II', 'elevation': 8035, 'range': 'Karakoram', 'location': 'Pakistan/China - Originally named K4'},
+    {'name': 'Shishapangma', 'elevation': 8027, 'range': 'Himalayas', 'location': 'China'},
+]
+
+function calculate() {
+    // This function will fire when the button in the HTML is clicked.
+}
