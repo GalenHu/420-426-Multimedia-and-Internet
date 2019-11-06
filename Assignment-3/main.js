3349ea12f5b9b43943c5221247db90927081a5f9
GalenHu <1858105@johnabbottcollege.net>
2019-11-06 13:44:34 -0500

    OK

--- Assignment-3/main.js
+++ Assignment-3/main.js
@@ -3,66 +3,32 @@ const nextLauch = "https://api.spacexdata.com/v3/launches/next";
 const pastLauch = "https://api.spacexdata.com/v3/launches/past?limit=3";
 const rockets = "https://api.spacexdata.com/v3/rockets";
 
+//Header
+const infoLink = "https://api.spacexdata.com/v3/info";
+ fetch(infoLink)
+			.then(response => response.json())
+			.then(info =>{
+					RequestInfo(info)
+			})
 
-fetch(url)
-    .then(response1 => response1.json())
-    .then(data =>{
-        doStuff1(data)
-    })
+			.catch(function(error){
+					//If there is any error you will catch them here
+					console.log(error);
+			});
 
-    .catch(function(error){
-        //If there is any error you will catch them here
-        console.log(error);
-    });
-
-function doStuff1(data){
-    //If the request was succesfull then data will have everything you asked for.
-    console.log(data)
-}
-
-fetch(nextLauch)
-    .then(response2 => response2.json())
-    .then(nLauch =>{
-        doStuff2(nLauch)
-    })
-
-    .catch(function(error){
-        //If there is any error you will catch them here
-        console.log(error);
-    });
-function doStuff2(nLauch){
-    //If the request was succesfull then data will have everything you asked for.
-    console.log(nLauch)
-
-    let next = document.getElementById("next");
-
-    next.innerHTML = nLauch.mission_name;
-}
-
-fetch(pastLauch)
-    .then(response3 => response3.json())
-    .then(pLauch =>{
-        doStuff3(pLauch)
-    })
-
-    .catch(function(error){
-        //If there is any error you will catch them here
-        console.log(error);
-    });
-
-function doStuff3(pLauch){
-    //If the request was succesfull then data will have everything you asked for.
-    console.log(pLauch)
-
-    let past = document.getElementById("past");
-    past.innerHTML = pLauch[0].flight_number
-}
+	function RequestInfo(info){
+			//If the request was succesfull then data will have everything you asked for.
+			console.log(info)
+			
+			document.getElementById("header_title").innerHTML = info.name
+			document.getElementById("header_summary").innerHTML = info.summary
+	}
 
 
-// fetch(rockets)
-//     .then(response4 => response4.json())
-//     .then(rocket =>{
-//         doStuff4(rocket)
+// fetch(pastLauch)
+//     .then(response3 => response3.json())
+//     .then(pLauch =>{
+//         doStuff3(pLauch)
 //     })
 
 //     .catch(function(error){
@@ -70,8 +36,28 @@ function doStuff3(pLauch){
 //         console.log(error);
 //     });
 
-// function doStuff4(rocket){
+// function doStuff3(pLauch){
 //     //If the request was succesfull then data will have everything you asked for.
-//     console.log(rocket)
+//     console.log(pLauch)
+
+//     let past = document.getElementById("past");
+//     past.innerHTML = pLauch[0].flight_number
 // }
+
+
+// // fetch(rockets)
+// //     .then(response4 => response4.json())
+// //     .then(rocket =>{
+// //         doStuff4(rocket)
+// //     })
+
+// //     .catch(function(error){
+// //         //If there is any error you will catch them here
+// //         console.log(error);
+// //     });
+
+// // function doStuff4(rocket){
+// //     //If the request was succesfull then data will have everything you asked for.
+// //     console.log(rocket)
+// // }
 
