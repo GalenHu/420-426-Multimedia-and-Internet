d424822d4e7e7a2623cdc789f7cb79c026d36c63
GalenHu <1858105@johnabbottcollege.net>
2019-11-15 14:18:34 -0500

    Working

--- Assignment-3/main.js
+++ Assignment-3/main.js
@@ -7,123 +7,122 @@ const rockets = "https://api.spacexdata.com/v3/rockets";
 
 //Header
 const infoLink = "https://api.spacexdata.com/v3/info";	//info
- fetch(infoLink)
-			.then(response => response.json())
-			.then(info =>{
-					RequestInfo(info)
-			})
+fetch(infoLink)
+	.then(response => response.json())
+	.then(info => {
+		RequestInfo(info)
+	})
 
-			.catch(function(error){
-					//If there is any error you will catch them here
-					console.log(error);
-			});
+	.catch(function (error) {
+		//If there is any error you will catch them here
+		console.log(error);
+	});
 
-	function RequestInfo(info){
-			//If the request was succesfull then data will have everything you asked for.
-			console.log(info)
-			
-			document.getElementById("header_summary").innerHTML = info.summary;
-			document.getElementById("founder").innerHTML = info.founder;
-			document.getElementById("founded").innerHTML = info.founded;
-	};
+function RequestInfo(info) {
+	//If the request was succesfull then data will have everything you asked for.
+	console.log(info)
 
-	//Timer of next lauch
+	document.getElementById("header_summary").innerHTML = info.summary;
+	document.getElementById("founder").innerHTML = info.founder;
+	document.getElementById("founded").innerHTML = info.founded;
+};
+
+//Timer of next lauch
 fetch("https://api.spacexdata.com/v3/launches/next") //next lauch
-    .then(response_next => response_next.json())
-    .then(next =>{
-        doStuff3(next)
-    })
+	.then(response_next => response_next.json())
+	.then(next => {
+		doStuff3(next)
+	})
 
-    .catch(function(error){
-        //If there is any error you will catch them here
-        console.log(error);
-    });
+	.catch(function (error) {
+		//If there is any error you will catch them here
+		console.log(error);
+	});
 
-function doStuff3(next){
-    //If the request was succesfull then data will have everything you asked for.
-    console.log(next)
+function doStuff3(next) {
+	//If the request was succesfull then data will have everything you asked for.
+	console.log(next)
 
 	let countDownDate = new Date(next.launch_date_utc).getTime();
 
-	let x = setInterval(function() {
+	let x = setInterval(function () {
 
 		// Get today's date and time
 		let now = new Date().getTime();
-		  
+
 		// Find the distance between now and the count down date
 		let distance = countDownDate - now;
-		  
+
 		// Time calculations for days, hours, minutes and seconds
 		let days = Math.floor(distance / (1000 * 60 * 60 * 24));
 		let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
 		let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
 		let seconds = Math.floor((distance % (1000 * 60)) / 1000);
-		  
+
 		// Output the result in an element with id="demo"
 		document.getElementById("next_launch_date_utc").innerHTML = days + "Day(s) " + hours + "Hour(s) "
-		+ minutes + "Minute(s) " + seconds + "Second(s) ";
-		  
+			+ minutes + "Minute(s) " + seconds + "Second(s) ";
+
 		// If the count down is over, write some text 
 		if (distance < 0) {
-		  clearInterval(x);
-		  document.getElementById("next_launch_date_utc").innerHTML = "EXPIRED";
+			clearInterval(x);
+			document.getElementById("next_launch_date_utc").innerHTML = "EXPIRED";
 		}
-	  }, 1000);
+	}, 1000);
 }
 
-const pastLauch = "https://api.spacexdata.com/v3/launches/past";
-fetch(pastLauch)
+let input = document.getElementById("pastInput");
+input.addEventListener("keyup", function (event) {
+	if (event.keyCode === 13) {
+		event.preventDefault();
+
+
+		const pastLauch = "https://api.spacexdata.com/v3/launches/past?limit=";
+		fetch(pastLauch + document.getElementById("pastInput").value)
 			.then(pastResponse => pastResponse.json())
-			.then(past =>{
-					RequestPast(past)
+			.then(past => {
+				RequestPast(past)
 			})
 
-			.catch(function(error){
-					//If there is any error you will catch them here
-					console.log(error);
+			.catch(function (error) {
+				//If there is any error you will catch them here
+				console.log(error);
 			});
 
-	function RequestPast(past){
-			//If the request was succesfull then data will have everything you asked for.
-			console.log(past)
-			var input = document.getElementById("pastInput");
-			input.addEventListener("keyup", function(event) {
-			if (event.keyCode === 13) {
-			event.preventDefault();
+	}
+});
 
-			let pastTable = document.getElementById("pastTable");
-			let pastNumber = document.getElementById("pastInput");
+function RequestPast(past) {
+	//If the request was succesfull then data will have everything you asked for.
+	console.log(past)
+	console.log(document.getElementById("pastInput").value);
+	
+	let pastTable = document.getElementById("pastTable");
+	pastTable.innerHTML = '';
 
-			for(let i =1; i< pastNumber; i++){
-				let tr = document.createElement('tr')
+	past.forEach(element => {
+		let tr = document.createElement('tr')
 
-			    let td1 = document.createElement('td');
-				let td2 = document.createElement('td');
-				let td3 = document.createElement('td');
-				let td4 = document.createElement('td');
+		let td1 = document.createElement('td');
+		let td2 = document.createElement('td');
+		let td3 = document.createElement('td');
+		let td4 = document.createElement('td');
 
-				let flightNumber = document.createTextNode(past.flight_number);
-				let lauchDate = document.createTextNode(past.lauch_date_unix);
-				let missionName = document.createTextNode(past.mission_name);
-				let detail = document.createTextNode(past.details);
+		let flightNumber = document.createTextNode(element.flight_number);
+		console.log(element.flight_number)
+		let lauchDate = document.createTextNode(element.launch_date_utc);
+		let missionName = document.createTextNode(element.mission_name);
+		let detail = document.createTextNode(element.details);
 
-				td1.appendChild(flightNumber);
-				td2.appendChild(lauchDate);
-				td3.appendChild(missionName);
-				td4.appendChild(detail);
-				tr.appendChild(td1);
-				tr.appendChild(td2);
-				tr.appendChild(td3);
-				tr.appendChild(td4);
+		td1.appendChild(flightNumber);
+		td2.appendChild(lauchDate);
+		td3.appendChild(missionName);
+		td4.appendChild(detail);
+		tr.appendChild(td1);
+		tr.appendChild(td2);
+		tr.appendChild(td3);
+		tr.appendChild(td4);
 
-				pastTable.appendChild(tr);
-			}
-			document.body.appendChild(pastTable);
-
-
-			}
-			});
-
-	};
-
-
+		pastTable.appendChild(tr);
+	});
+}
