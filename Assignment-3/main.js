04c0240c2fc2a3cdb520af072752b724884e4bbb
Galen Hu <1858105@johnabbottcollege.net>
2019-11-17 23:07:57 -0500

    finish

--- Assignment-3/main.js
+++ Assignment-3/main.js
@@ -1,77 +1,140 @@
-const url = "https://api.spacexdata.com/v3/"; //The root of the SpaceX API
-const nextLauch = "https://api.spacexdata.com/v3/launches/next";
-const pastLauch = "https://api.spacexdata.com/v3/launches/past?limit=3";
-const rockets = "https://api.spacexdata.com/v3/rockets";
+M.AutoInit();
 
+/******************************* INDEX.HTML **************************************/
+const infoLink = "https://api.spacexdata.com/v3/info";	//info
+fetch(infoLink)
+	.then(response => response.json())
+	.then(info => {
+		RequestInfo(info)
+	})
 
-fetch(url)
-    .then(response1 => response1.json())
-    .then(data =>{
-        doStuff1(data)
-    })
+	.catch(function (error) {
+		//If there is any error you will catch them here
+		console.log(error);
+	});
 
-    .catch(function(error){
-        //If there is any error you will catch them here
-        console.log(error);
-    });
+function RequestInfo(info) {
+	//If the request was succesfull then data will have everything you asked for.
 
-function doStuff1(data){
-    //If the request was succesfull then data will have everything you asked for.
-    console.log(data)
+	document.getElementById("header_summary").innerHTML = info.summary;
+	document.getElementById("founder").innerHTML = info.founder;
+    document.getElementById("founded").innerHTML = info.founded;
+    document.getElementById("ceo").innerHTML = info.ceo;
+    document.getElementById("employees").innerHTML = info.employees;
+};
+/******************************* INDEX.HTML **************************************/
+
+/******************************* NEXT.HTML **************************************/
+fetch("https://api.spacexdata.com/v3/launches/next") //next lauch
+	.then(response_next => response_next.json())
+	.then(next => {
+		doStuff3(next)
+	})
+
+	.catch(function (error) {
+		//If there is any error you will catch them here
+		console.log(error);
+	});
+
+function doStuff3(next) {
+	//If the request was succesfull then data will have everything you asked for.
+
+    //countdown
+	let countDownDate = new Date(next.launch_date_utc).getTime();
+
+	let x = setInterval(function () {
+
+		// Get today's date and time
+		let now = new Date().getTime();
+
+		// Find the distance between now and the count down date
+		let distance = countDownDate - now;
+
+		// Time calculations for days, hours, minutes and seconds
+		let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+		let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+		let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+		let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+		// Output the result
+		document.getElementById("next_launch_date_utc").innerHTML = days + " : " + hours + " : "
+			+ minutes + " : " + seconds;
+
+		// If the count down is over, write some text 
+		if (distance < 0) {
+			clearInterval(x);
+			document.getElementById("next_launch_date_utc").innerHTML = "EXPIRED";
+        }
+        
+        
+    }, 1000);
+    //end of countdown
+
+    document.getElementById("mission_name").innerHTML = next.mission_name;
 }
+/******************************* END NEXT.HTML **************************************/
 
-fetch(nextLauch)
-    .then(response2 => response2.json())
-    .then(nLauch =>{
-        doStuff2(nLauch)
-    })
+/******************************* PASTLAUCH.HTML **************************************/
+let input = document.getElementById("pastInput");
+input.addEventListener("keyup", function (event) {
+	if (event.keyCode === 13) {
+		event.preventDefault();
 
-    .catch(function(error){
-        //If there is any error you will catch them here
-        console.log(error);
-    });
-function doStuff2(nLauch){
-    //If the request was succesfull then data will have everything you asked for.
-    console.log(nLauch)
 
-    let next = document.getElementById("next");
+		const pastLauch = "https://api.spacexdata.com/v3/launches/past?limit=";
+		fetch(pastLauch + document.getElementById("pastInput").value)
+			.then(pastResponse => pastResponse.json())
+			.then(past => {
+				RequestPast(past)
+			})
 
-    next.innerHTML = nLauch.mission_name;
+			.catch(function (error) {
+				//If there is any error you will catch them here
+				console.log(error);
+			});
+
+	}
+});
+
+function RequestPast(past) {
+	//If the request was succesfull then data will have everything you asked for.
+	console.log(document.getElementById("pastInput").value);
+	
+    let pastTable = document.getElementById("pastTable");
+    console.log(pastTable);
+	pastTable.innerHTML = '';
+
+	past.forEach(element => {
+		let tr = document.createElement('tr')
+
+		let td1 = document.createElement('td');
+		let td2 = document.createElement('td');
+		let td3 = document.createElement('td');
+		let td4 = document.createElement('td');
+
+		let flightNumber = document.createTextNode(element.flight_number);
+        let timestamp = element.launch_date_unix;
+        date = new Date(timestamp * 1000),
+        datevalues = [
+        date.getFullYear(),
+        date.getMonth()+1,
+        date.getDate()
+        ];
+
+        let lauchDate = document.createTextNode(`${datevalues[0]}/${datevalues[1]}/${datevalues[2]}`);
+		let missionName = document.createTextNode(element.mission_name);
+		let detail = document.createTextNode(element.details);
+
+		td1.appendChild(flightNumber);
+		td2.appendChild(lauchDate);
+		td3.appendChild(missionName);
+		td4.appendChild(detail);
+		tr.appendChild(td1);
+		tr.appendChild(td2);
+		tr.appendChild(td3);
+		tr.appendChild(td4);
+
+        pastTable.appendChild(tr);
+	});
 }
-
-fetch(pastLauch)
-    .then(response3 => response3.json())
-    .then(pLauch =>{
-        doStuff3(pLauch)
-    })
-
-    .catch(function(error){
-        //If there is any error you will catch them here
-        console.log(error);
-    });
-
-function doStuff3(pLauch){
-    //If the request was succesfull then data will have everything you asked for.
-    console.log(pLauch)
-
-    let past = document.getElementById("past");
-    past.innerHTML = pLauch[0].flight_number
-}
-
-
-// fetch(rockets)
-//     .then(response4 => response4.json())
-//     .then(rocket =>{
-//         doStuff4(rocket)
-//     })
-
-//     .catch(function(error){
-//         //If there is any error you will catch them here
-//         console.log(error);
-//     });
-
-// function doStuff4(rocket){
-//     //If the request was succesfull then data will have everything you asked for.
-//     console.log(rocket)
-// }
-
+/******************************* END PASTLAUCH.HTML **************************************/
---
+++ Assignment-3/pastlauch.js
@@ -0,0 +1,53 @@
+//this .js was create because for some reason, when i put this in main.js, it doesnt work
+/******************************* ROCKETS.HTML **************************************/
+
+const rocketLink = "https://api.spacexdata.com/v3/rockets";
+
+
+fetch(rocketLink)
+	.then(getRocket => getRocket.json())
+	.then(rocket => {
+		RequestRocket(rocket)
+	})
+
+	.catch(function (error) {
+		//If there is any error you will catch them here
+		console.log(error);
+	});
+
+function RequestRocket(rocket) {
+	//If the request was succesfull then data will have everything you asked for.
+
+    let rocketsTable = document.getElementById("rocketsTable");
+    console.log("hey");
+    console.log(rocket);
+
+    document.getElementById("name1").innerHTML = rocket[0].rocket_name;
+    document.getElementById("desc1").innerHTML = rocket[0].description;
+    document.getElementById("flight1").innerHTML = rocket[0].first_flight;
+    document.getElementById("succ1").innerHTML = "Success Rate: " + rocket[0].success_rate_pct;
+    document.getElementById("cost1").innerHTML = "Cost per launch: " + rocket[0].cost_per_launch;
+    document.getElementById("wiki1").innerHTML = "Wikipedia: " + rocket[0].wikipedia;
+
+    document.getElementById("name2").innerHTML = rocket[1].rocket_name;
+    document.getElementById("desc2").innerHTML = rocket[1].description;
+    document.getElementById("flight2").innerHTML = rocket[1].first_flight;
+    document.getElementById("succ2").innerHTML = "Success Rate: " + rocket[1].success_rate_pct;
+    document.getElementById("cost2").innerHTML = "Cost per launch: " + rocket[1].cost_per_launch;
+    document.getElementById("wiki2").innerHTML = "Wikipedia: " + rocket[1].wikipedia;
+
+    document.getElementById("name3").innerHTML = rocket[2].rocket_name;
+    document.getElementById("desc3").innerHTML = rocket[2].description;
+    document.getElementById("flight3").innerHTML = rocket[2].first_flight;
+    document.getElementById("succ3").innerHTML = "Success Rate: " + rocket[2].success_rate_pct;
+    document.getElementById("cost3").innerHTML = "Cost per launch: " + rocket[2].cost_per_launch;
+    document.getElementById("wiki3").innerHTML = "Wikipedia: " + rocket[2].wikipedia;
+
+    document.getElementById("name4").innerHTML = rocket[3].rocket_name;
+    document.getElementById("desc4").innerHTML = rocket[3].description;
+    document.getElementById("flight4").innerHTML = rocket[3].first_flight;
+    document.getElementById("succ4").innerHTML = "Success Rate: " + rocket[3].success_rate_pct;
+    document.getElementById("cost4").innerHTML = "Cost per launch: " + rocket[3].cost_per_launch;
+    document.getElementById("wiki4").innerHTML = "Wikipedia: " + rocket[3].wikipedia;
+
+};
