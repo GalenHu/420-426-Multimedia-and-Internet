43f2b40021743982035345d8c9c816a34c4e66ac
GalenHu <55711983+GalenHu@users.noreply.github.com>
2019-11-26 09:59:13 -0500

    Update main.js

--- Game-GALEN-HU/main.js
+++ Game-GALEN-HU/main.js
@@ -79,6 +79,7 @@ class Character {
 
 class Solid {
   x1;x2;y1;y2;
+  obsColor = "none";
 
   constructor(x1, y1, x2, y2) {
     this.x1 = x1;
@@ -89,8 +90,12 @@ class Solid {
 
   drawPlat() {
     context.beginPath();
+    context.fillStyle = this.obsColor
     context.rect(this.x1, this.y1, this.x2, this.y2);
+    context.closePath();
+    context.fill();
     context.stroke();
+
   }
 
   update() {
@@ -99,11 +104,16 @@ class Solid {
 
 }
 
+obs1 = new Solid(0, 0, 160, 500);
+obs2 = new Solid(300,55, 500, 500);
+obs1.drawPlat();
+obs2.drawPlat();
+
 mychar = new Character(300, 75);
 mychar.drawChar();
 
-myplat = new Solid(20, 20, 150, 150);
-myplat.drawPlat();
+
+
 
 
 
@@ -115,9 +125,11 @@ function animate() {
   window.addEventListener("keydown", doKeyDown); //add false parameter?
   window.addEventListener("keyup", doKeyUp, false); //add false parameter?
   document.getElementById("myCanvas").onmousemove = findObjectCoords;
-  intersect(mychar,myplat);
+  intersect(mychar,obs1);
+  intersect(mychar,obs2);
   mychar.update();
-  myplat.update();
+  obs1.update();
+  obs2.update();
 }
 
 animate();
@@ -129,14 +141,12 @@ function intersect(character, obstacle){
   myRight = character.xPosition+character.radius;
   myTop = character.yPosition-character.radius;
   myBottom = character.yPosition+character.radius;
-  if(myRight>obstacle.x1 && myLeft < obstacle.x2+20 && myBottom > obstacle.y1 && myTop < obstacle.y2+20){
+  if(myRight>obstacle.x1 && myLeft < obstacle.x2 && myBottom > obstacle.y1 && myTop < obstacle.y2){
     console.log(true);
-    
     character.contact(true, "blue");
     character.speed = 0.2;
   }
   else{
-    console.log(false);
     character.contact(false, "red");
     character.speed = 5;
   }
